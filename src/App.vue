f96050bdedf6fe59374f5abacb7f73bfd113c3e0
hamdi hamza <hamdihamzadev@hotmail.com>
2025-02-28 19:25:15 +0000

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <b-container>
+      <b-row>
+        <b-col v-for="item in 12" :key="item" >
+          <span>hamza</span>
+        </b-col>
+      </b-row>
+    </b-container>
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +15,9 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+#app{
+  color: rgb(31, 31, 31);
 }
 </style>
